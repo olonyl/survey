8d46bd0fe3fa09366786f5ccb134badfe595b017
Olonyl Horacio Rocha Landeros <jose87.landeros@gmail.com>
2018-12-14 09:00:44 -0600

    Endpoint changed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import MainForm from './components/MainForm';
 import { setContext } from 'apollo-link-context';
 
-const baseEndpointURL = 'http://ec2-18-223-97-7.us-east-2.compute.amazonaws.com:5000';
+//const baseEndpointURL = 'http://ec2-18-223-97-7.us-east-2.compute.amazonaws.com:5000';
+const baseEndpointURL = 'http://ec2-52-14-244-20.us-east-2.compute.amazonaws.com:5000';
 
 const token =
 	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7IklkIjoxMCwiQ29kZV9Vc2VyIjoiYWRtaW4gICAgICJ9LCJpYXQiOjE1Mzg2NjI4ODgsImV4cCI6MTg1NDIzODg4OH0.3p2Hej6LhKeiNvONYNsJ2S7-5NSeeC-gcKgYyJvc8F0';
